7b41975d431a02af3e3d5e35e69c1816a47a81a9
Adilson de Brito <abrito10@gmail.com>
2021-05-05 13:37:22 -0300

    Layers

---
+++ backend/src/main/java/com/abrito10/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.abrito10.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abrito10.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller,Long>{
+
+}
---
+++ backend/src/main/java/com/abrito10/dsvendas/services/SellerService.java
@@ -0,0 +1,27 @@
+package com.abrito10.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.abrito10.dsvendas.dto.SellerDTO;
+import com.abrito10.dsvendas.entities.Seller;
+import com.abrito10.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+
+}
